e41ea673eb56323a4ea6e0fd6b5ccf47f1b6e840
Bauke Westendorp <baukewestendorp@gmail.com>
2025-12-24 17:55:19 +0100

    Remove `thiserror` dependency
    This halves (honesly already short) the build time.

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,39 @@
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error("I/O error: {0}")]
-    Io(#[from] std::io::Error),
+use std::{error, fmt, io};
 
-    #[error("Driver error: {message}")]
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+
     Driver { message: &'static str },
 
-    #[error("HID device not found")]
     HidDeviceNotFound,
-    #[error("HID API already initialized")]
     HidApiAlreadyInitialized,
-    #[error("cannot open HID device")]
     CannotOpenHidDevice,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::Io(e) => write!(f, "I/O error: {}", e),
+            Error::Driver { message } => write!(f, "Driver error: {}", message),
+            Error::HidDeviceNotFound => write!(f, "HID device not found"),
+            Error::HidApiAlreadyInitialized => write!(f, "HID API already initialized"),
+            Error::CannotOpenHidDevice => write!(f, "cannot open HID device"),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match self {
+            Error::Io(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::Io(err)
+    }
 }
